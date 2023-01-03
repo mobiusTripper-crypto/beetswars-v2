72cf436c5e5424db642aa09c8debc3635e94f4e9
mobiusTripper-crypto <99622829+mobiusTripper-crypto@users.noreply.github.com>
2023-01-02 20:03:56 -0500

    fix icon names

--- src/components/ColorModeSwitcher.tsx
+++ src/components/ColorModeSwitcher.tsx
@@ -5,22 +5,20 @@ import {
   IconButton,
   IconButtonProps,
 } from "@chakra-ui/react";
-import { FaMoon, FaSun } from "react-icons/fa";
+import { FaMoon as MoonIcon, FaSun as SunIcon } from "react-icons/fa";
 
 type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">;
 
 export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue("dark", "light");
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
 
   return (
     <IconButton
-      size="md"
       fontSize="lg"
       variant="ghost"
-      color="current"
-      marginLeft="2"
+      ml="2"
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
       aria-label={`Switch to ${text} mode`}
