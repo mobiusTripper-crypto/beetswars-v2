b7a6c00107cb22055c148f81a3b6f7f51b5addd0
Johannes Heinemann <defi@onkeljoe.de>
2023-01-06 15:16:58 +0100

    add login database

---
+++ src/utils/database/loginID.db.ts
@@ -0,0 +1,32 @@
+import clientPromise from "./mongodb";
+
+type Login = { key: string };
+
+export async function readLoginList(): Promise<string[]> {
+  try {
+    const client = await clientPromise;
+    const coll = client.db("beetswars").collection<Login>("login");
+    const items = await coll.find<Login>({}).toArray(); // find all
+    if (!items || items.length === 0) return [];
+    const keylist = items.map((item) => item.key);
+    return keylist;
+  } catch (error) {
+    return [];
+  }
+}
+
+// will not insert duplicate values
+export async function addLogin(key: string): Promise<boolean> {
+  try {
+    const client = await clientPromise;
+    const coll = client.db("beetswars").collection<Login>("login");
+    const { value, ok } = await coll.findOneAndReplace(
+      { key }, // find
+      { key }, // replace with
+      { upsert: true, returnDocument: "after" } // add if not found
+    );
+    return !!ok && !!value;
+  } catch (error) {
+    return false;
+  }
+}
