b00f9b7213f3dae42f1c15e95d81bfa93460beef
mobiusTripper-crypto <99622829+mobiusTripper-crypto@users.noreply.github.com>
2023-01-07 08:53:11 -0500

    nextauth signin authenticate on server side

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,39 @@
-import NextAuth from "next-auth";
+import NextAuth, { type AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { readLoginList } from "utils/database/loginID.db";
 
 const useSecureCookies = !!process.env.VERCEL_URL;
+async function userlist() {
+  return await readLoginList();
+}
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
   ],
-  // This code is only necessary for subdomains
+  callbacks: {
+    async signIn({ user }) {
+      const isAllowedToSignIn = (await userlist()).includes(user.name || "");
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        // Return false to display a default error message
+        return false;
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
+      }
+    },
+    // if modification of the callbackUrl is required us this
+    // async redirect({ url, baseUrl }) {
+    //   console.log(url, baseUrl);
+    //   return url;
+    // },
+  },
 
+  // This code is only necessary for subdomains
   cookies: {
     sessionToken: {
       name: `${useSecureCookies ? "__Secure-" : ""}next-auth.session-token`,
@@ -24,4 +46,5 @@ export const authOptions = {
     },
   },
 };
+
 export default NextAuth(authOptions);
--- src/server/trpc/router/loginId.ts
+++ src/server/trpc/router/loginId.ts
@@ -1,17 +1,18 @@
+import { TRPCError } from "@trpc/server";
+import { type Session } from "next-auth";
 import { addLogin, readLoginList } from "utils/database/loginID.db";
 import { z } from "zod";
 
 import { router, publicProcedure } from "../trpc";
 
 export const loginRouter = router({
-  list: publicProcedure.query(async () => {
-    return {
-      userlist: await readLoginList(),
-    };
-  }),
   addUser: publicProcedure
     .input(z.object({ key: z.string() }))
-    .mutation(async ({ input }) => {
+    .mutation(async ({ input, ctx }) => {
+      const session = ctx as Session;
+      if (!session.user) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
       const result = await addLogin(input.key);
       return result;
     }),
