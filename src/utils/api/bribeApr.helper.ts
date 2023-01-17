e30abc094edebfebad9880bc143e8beea3351921
Johannes Heinemann <defi@onkeljoe.de>
2023-01-17 21:08:45 +0100

    add emissions API

---
+++ src/utils/api/bribeApr.helper.ts
@@ -0,0 +1,106 @@
+import { readOneChartdata } from "utils/database/chartdata.db";
+import { getCoingeckoCurrentPrice } from "utils/externalData/coingecko";
+import { getBlockByTs } from "utils/externalData/ftmScan";
+import { getBeetsPerBlock } from "utils/externalData/theGraph";
+
+export interface EmissionData {
+  round: string;
+  emission: number;
+  beetsPrice: number;
+  usdValue: number;
+  voteEmission: number;
+  percentUsdValue: number;
+}
+
+export async function getEmissionForRound(
+  round: string
+): Promise<EmissionData | null> {
+  // start at Wed Jan 12, 2022 - the start of round 1 payout (I guess)
+  const ROUND1 = 1641988800; // timestamp of 12-01-2022 12:00 pm UTC as reference point
+  const TWOWEEKS = 14 * 24 * 60 * 60; // seconds
+  const ts1 = ROUND1 + (+round - 1) * TWOWEEKS; // from end of previous round
+  const ts2 = ts1 + TWOWEEKS; // to end of given round
+  const now = Math.round(Date.now() / 1000);
+  let start = 0;
+  let end = 0;
+  let factor = 1; // to expand shorter timespan to full 14 days
+
+  // switch cases
+  // A) emission is finished (ts2 <= now)
+  if (ts2 <= now) {
+    console.log("finished");
+    start = ts1;
+    end = ts2;
+  }
+  // B) emission has started but not finished (ts1 < date.now, ts2 > date.now) take emissions from start to now and expand to 14 days
+  else if (ts1 < now) {
+    console.log("active");
+    start = ts1;
+    end = now;
+    factor = TWOWEEKS / (end - start); // expand to full 14 days
+  }
+  // C) emission has not yet started (ts1 > date.now) take emissions of last 1 day * 14
+  else {
+    console.log("future");
+    start = now - 24 * 60 * 60; //one day back
+    end = now;
+    factor = 14;
+  }
+  const block1 = await getBlockByTs(start);
+  const block2 = await getBlockByTs(end);
+  const beets1 = await getBeetsPerBlock(block1);
+  const beets2 = await getBeetsPerBlock(block2);
+  const blockCount = block2 - block1;
+  // early return on error
+  if (!beets1 || !beets2 || !blockCount) return null;
+  let emission = blockCount * beets1 * factor;
+  // if emission changed during epoch
+  if (beets1 !== beets2) {
+    const changeBlock = await findEmissionChangeBlock(block1, block2);
+    const part1 = (changeBlock - block1) * beets1;
+    const part2 = (block2 - changeBlock) * beets2;
+    emission = (part1 + part2) * factor;
+  }
+  const beetsPrice = await findBeetsPrice(round);
+  const usdValue = emission * beetsPrice;
+  const voteEmission = emission * 0.872 * 0.3; // 30% of 87.2% of emissions
+  const percentUsdValue = (voteEmission * beetsPrice) / 100;
+  return {
+    round,
+    emission,
+    beetsPrice,
+    usdValue,
+    voteEmission,
+    percentUsdValue,
+  };
+}
+
+// find block by continuous halving
+async function findEmissionChangeBlock(
+  lowBlock: number,
+  highBlock: number
+): Promise<number> {
+  var beets1 = await getBeetsPerBlock(lowBlock);
+  var beets2 = await getBeetsPerBlock(highBlock);
+  var beets3 = 0;
+  if (beets1 === beets2) return highBlock + 1; // return early if no change
+  var mid = Math.round((lowBlock + highBlock) / 2);
+  do {
+    beets3 = await getBeetsPerBlock(mid);
+    if (beets3 === beets1) {
+      lowBlock = mid;
+    } else {
+      highBlock = mid;
+    }
+    mid = Math.round((lowBlock + highBlock) / 2);
+  } while (lowBlock + 1 !== highBlock);
+  return highBlock;
+}
+
+// find BEETS price from database or from live source
+async function findBeetsPrice(round: string): Promise<number> {
+  const chartdata = await readOneChartdata(round);
+  if (!!chartdata) return chartdata.priceBeets;
+  const cg = await getCoingeckoCurrentPrice("beethoven-x");
+  return cg;
+}
