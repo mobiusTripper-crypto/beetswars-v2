5cf13ec5efaa813a195368c8dd07a58916d87b6f
Johannes Heinemann <defi@onkeljoe.de>
2023-01-27 20:18:13 +0100

    fix some more prettier/linter warnings

--- src/pages/round/index.tsx
+++ src/pages/round/index.tsx
@@ -2,10 +2,7 @@ import { useEffect } from "react";
 import Router from 'next/router'
 
 export default function Redirect() {
-
       useEffect(() => {
             Router.push('/round/latest')
       });
-
 }
-
