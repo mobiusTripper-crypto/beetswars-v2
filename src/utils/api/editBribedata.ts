72352ca470604929ab1eb0358b060b1af6b9a4b6
Johannes Heinemann <defi@onkeljoe.de>
2023-01-30 12:57:45 +0100

    addOffer, editOffer, deleteOffer

--- src/utils/api/editBribedata.ts
+++ src/utils/api/editBribedata.ts
@@ -82,16 +82,70 @@ export async function deleteToken(
   }
 }
 
-export async function addEditOffer(
+export async function addOffer(
   payload: Bribedata,
   round: number
-): Promise<Bribedata | null> {
-  return null;
+): Promise<Bribedata[] | null> {
+  try {
+    const newOffer = Bribedata.parse(payload);
+    const bribefile = await readOneBribefile(round);
+    if (!bribefile) return null;
+    if (bribefile.bribedata.length === 0) {
+      newOffer.offerId = 1;
+    } else {
+      newOffer.offerId =
+        bribefile.bribedata.reduce((prev, current) => {
+          return prev.offerId > current.offerId ? prev : current;
+        }).offerId + 1;
+    }
+    bribefile.bribedata.push(newOffer);
+    const result = await insertBribefile(bribefile, round);
+    if (!result) return null;
+    return result.bribedata;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function editOffer(
+  payload: Bribedata,
+  round: number
+): Promise<Bribedata[] | null> {
+  try {
+    const newOffer = Bribedata.parse(payload);
+    const bribefile = await readOneBribefile(round);
+    if (!bribefile) return null;
+    const { bribedata, ...rest } = bribefile;
+    if (bribedata.length === 0) return null;
+    const bribeArray = bribedata.map((item) => {
+      return item.offerId === newOffer.offerId ? newOffer : item;
+    });
+    const newPayload = { ...rest, bribedata: bribeArray };
+    const result = await insertBribefile(newPayload, round);
+    if (!result?.bribedata) return null;
+    return result.bribedata;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 export async function deleteOffer(
   offerId: number,
   round: number
 ): Promise<boolean> {
-  return false;
+  try {
+    const bribefile = await readOneBribefile(round);
+    if (!bribefile) return false;
+    const newOffers = bribefile.bribedata.filter(
+      (offer) => offer.offerId !== offerId
+    );
+    const newBribefile = { ...bribefile, bribedata: newOffers };
+    const result = await insertBribefile(newBribefile, round);
+    return !!result;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
