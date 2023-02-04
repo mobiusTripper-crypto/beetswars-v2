c048981b37dd2569dfdbee7729c923d095a9c505
Johannes Heinemann <defi@onkeljoe.de>
2023-02-04 19:26:55 +0100

    additional roundlist in pure number

--- src/components/RoundSelector.tsx
+++ src/components/RoundSelector.tsx
@@ -1,5 +1,6 @@
 import { trpc } from "utils/trpc";
 import { useGlobalContext } from "contexts/GlobalContext";
+import { Select } from "@chakra-ui/react";
 
 // definition to avoid "any"
 interface RoundSelectorProps {
@@ -22,14 +23,24 @@ export function RoundSelector({ handleChange = defaultHandleChange }: RoundSelec
     latest: 0,
   };
 
+  // // TODO: Use this new endpoint to get {rounds: number[], latest: number}
+  ////////////////////////////////////////////////////////////////////////////
+  // const roundList = trpc.rounds.list.useQuery(undefined, {
+  //   refetchOnWindowFocus: false,
+  // }).data?.data ?? {
+  //   rounds: [] as number[],
+  //   latest: 0,
+  // };
+
   return (
-    <select onChange={handleChange} value={requestedRound}>
+    // TODO: select or Select ?
+    <Select onChange={handleChange} value={requestedRound}>
       {roundList.rounds.map((round: string, index: number) => (
         <option key={index} value={round}>
           Round {round}
         </option>
       ))}
-    </select>
+    </Select>
   );
 }
 
--- src/types/roundlist.trpc.ts
+++ src/types/roundlist.trpc.ts
@@ -6,3 +6,10 @@ export const Roundlist = z.object({
 });
 
 export type Roundlist = z.infer<typeof Roundlist>;
+
+export const RoundlistNum = z.object({
+  rounds: z.number().array(),
+  latest: z.number(),
+});
+
+export type RoundlistNum = z.infer<typeof RoundlistNum>;
