fb1a9e46de2c2afbe174aba2cbb6c6e010cb4aa4
Johannes Heinemann <defi@onkeljoe.de>
2023-02-06 18:07:19 +0100

    update failed temp file

--- src/utils/api/updateTemp.helper.ts
+++ src/utils/api/updateTemp.helper.ts
@@ -1,4 +1,4 @@
-import { readAllChartdata, updateChartdata } from "utils/database/chartdata.db";
+import { readAllChartdata } from "utils/database/chartdata.db";
 import { getEmissionForRound } from "./bribeApr.helper";
 
 export async function updateAllChartdata() {
@@ -6,11 +6,12 @@ export async function updateAllChartdata() {
   if (!chartdata) return null;
   for (const singlechart of chartdata) {
     const round = singlechart.round;
-    const data = await getEmissionForRound(round);
+    const data = await getEmissionForRound(+round);
     let roi = data?.avgBribeRoiInPercent ?? 0;
     if (isNaN(roi)) roi = 0;
     console.log("roi: ", roi);
-    const res = await updateChartdata(round, roi);
+    // const res = await updateChartdata(round, roi);
+    const res = { round: null };
     console.log(res?.round ?? "00");
   }
   const results = await readAllChartdata();
