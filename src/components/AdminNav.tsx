fdd7d6f1094a44f755ba0452f6fca7d0cc19ab26
RnZ3 <104623404+RnZ3@users.noreply.github.com>
2023-02-25 14:08:19 +0100

    hilight nav items ... WIP

--- src/components/AdminNav.tsx
+++ src/components/AdminNav.tsx
@@ -1,22 +1,29 @@
 import { Link, Card, Button, HStack } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 export const AdminNav = () => {
+  const router = useRouter();
+  const currentPath = router.pathname;
+  const adminPath = "/admin";
+  const bribeformPath = "/bribeform";
+  const poolPath = "/editVotablePools";
+
   return (
-      <>
-        <Card m={6} p={2}>
-          <HStack gap={6}>
-            <Link as={NextLink} href="/admin">
-              <Button>Admin</Button>
-            </Link>
-            <Link as={NextLink} href="/bribeform">
-              <Button>Bribe Forms</Button>
-            </Link>
-            <Link as={NextLink} href="/editVotablePools">
-              <Button>Edit votable Pools</Button>
-            </Link>
-          </HStack>
-        </Card>
+    <>
+      <Card m={6} p={2}>
+        <HStack gap={6}>
+          <Link as={NextLink} href={adminPath}>
+            <Button isActive={currentPath === adminPath ? true : false}>Admin</Button>
+          </Link>
+          <Link as={NextLink} href={bribeformPath}>
+            <Button isActive={currentPath === bribeformPath ? true : false}>Bribe Forms</Button>
+          </Link>
+          <Link as={NextLink} href={poolPath}>
+            <Button isActive={currentPath === poolPath ? true : false}>Edit votable Pools</Button>
+          </Link>
+        </HStack>
+      </Card>
     </>
   );
 };
