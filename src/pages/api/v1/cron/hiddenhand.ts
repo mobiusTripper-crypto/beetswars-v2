0fc387b7665b38697d5a2487dbdda4bf39442d00
Johannes Heinemann <defi@onkeljoe.de>
2024-02-18 22:29:15 +0100

    add REST API endpoint

---
+++ src/pages/api/v1/cron/hiddenhand.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import processHiddenhandApi from "utils/api/hiddenhand.helper";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") return res.status(501).send("not implemented");
+  const data = await processHiddenhandApi();
+  if (!data) return res.status(404).send("No object with given ID found");
+  res.json(data);
+}
