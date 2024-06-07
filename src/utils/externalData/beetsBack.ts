bf89687b00a54c4b081fa1879c93bcc07d07824f
Johannes Heinemann <defi@onkeljoe.de>
2024-06-07 18:06:43 +0200

    Revert "add caching to beets price getters"
    This reverts commit 12107f55e885dbdf98fc1c763432b0296a77a770.

--- src/utils/externalData/beetsBack.ts
+++ src/utils/externalData/beetsBack.ts
@@ -18,22 +18,9 @@ export async function getTokenPrice(timestamp: number, address: string): Promise
     }
   `;
   try {
-    // const { tokenGetPriceChartData } = (await request(queryUrl, query)) as {
-    //   tokenGetPriceChartData: Chartdata[];
-    // };
-
-    const { tokenGetPriceChartData } = (await fetch(queryUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    }).then(res => res.json())) as {
+    const { tokenGetPriceChartData } = (await request(queryUrl, query)) as {
       tokenGetPriceChartData: Chartdata[];
     };
-
     const result = tokenGetPriceChartData.reduce(
       (max, current) => {
         if (current.timestamp <= timestamp && current.timestamp > max.timestamp) {
@@ -67,22 +54,9 @@ export async function getPoolPriceHist(timestamp: number, address: string): Prom
     }
   `;
   try {
-    // const { poolGetSnapshots } = (await request(queryUrl, query)) as {
-    //   poolGetSnapshots: PoolChartdata[];
-    // };
-
-    const { poolGetSnapshots } = (await fetch(queryUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    }).then(res => res.json())) as {
+    const { poolGetSnapshots } = (await request(queryUrl, query)) as {
       poolGetSnapshots: PoolChartdata[];
     };
-
     const result = poolGetSnapshots.reduce(
       (max, current) => {
         if (Number(current.timestamp) <= timestamp && Number(current.timestamp) > max.timestamp) {
@@ -122,22 +96,9 @@ export async function getPoolPriceLive(address: string): Promise<number> {
     }
   `;
   try {
-    // const { poolGetPool } = (await request(queryUrl, query)) as {
-    //   poolGetPool: PoolData;
-    // };
-
-    const { poolGetPool } = (await fetch(queryUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    }).then(res => res.json())) as {
-      poolGetPool: PoolData[];
+    const { poolGetPool } = (await request(queryUrl, query)) as {
+      poolGetPool: PoolData;
     };
-
     const result =
       Number(poolGetPool.dynamicData.totalLiquidity) / Number(poolGetPool.dynamicData.totalShares);
     return result;
@@ -150,35 +111,25 @@ export async function getPoolPriceLive(address: string): Promise<number> {
 // get current price for single token
 export async function getTokenPriceLive(address: string): Promise<number> {
   type TokenData = {
-    address: string;
-    price: number;
+      address: string;
+      price: number;
   };
   const queryUrl = "https://backend-v3.beets-ftm-node.com/graphql";
   const query = gql`
-    query TokenPrice {
-      tokenGetCurrentPrices(chains: FANTOM) {
-        address
-        price
-      }
+  query TokenPrice {
+    tokenGetCurrentPrices(
+      chains: FANTOM
+    )
+    {
+      address
+      price
     }
+  }
   `;
   try {
-    // const { tokenGetCurrentPrices } = (await request(queryUrl, query)) as {
-    //   tokenGetCurrentPrices: TokenData[];
-    // };
-
-    const { tokenGetCurrentPrices } = (await fetch(queryUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: query,
-      }),
-    }).then(res => res.json())) as {
+    const { tokenGetCurrentPrices } = (await request(queryUrl, query)) as {
       tokenGetCurrentPrices: TokenData[];
     };
-
     const token = tokenGetCurrentPrices.find(tkn => tkn.address == address);
     const result = token?.price || 0;
     return result;
